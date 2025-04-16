59b568f149a6cd5da33ee79ad976155422df9b09
pdxpro0098 <125691886+pdxpro0098@users.noreply.github.com>
2025-04-16 16:27:14 +0530

    posted solution 1.c

--- Solutions File/1.c
+++ Solutions File/1.c
@@ -1,1 +1,29 @@
+#include <stdio.h>
 
+void isPrime(int prime);
+
+int main()
+{
+
+    for (int i = 100; i <= 500; i++)
+    {
+        isPrime(i);
+    }
+    return 0;
+}
+
+void isPrime(int prime)
+{
+    int divCount = 0;
+    for (int i = 1; i <= prime; i++)
+    {
+        if (prime % i == 0)
+        {
+            divCount++;
+        }
+    }
+    if (divCount == 2)
+    {
+        printf("%d is prime \n", prime);
+    }
+}
