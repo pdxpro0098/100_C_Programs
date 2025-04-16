91396d3a9bc14cdb663b8d4af22ff1b49d2ad512
pdxpro0098 <125691886+pdxpro0098@users.noreply.github.com>
2025-04-16 17:56:56 +0530

    Create 9.c

---
+++ Solutions File/9.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+static int fact(int num)
+{
+    int fact = 1;
+    for (int i = 1; i <= num; i++)
+    {
+        fact *= i;
+    }
+    return fact;
+}
+
+int main()
+{
+    printf("%d", fact(5));
+
+    return 0;
+}
