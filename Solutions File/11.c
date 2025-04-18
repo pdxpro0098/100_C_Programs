897baa21db3ae0f16ee67de111bd5151fbd84a1a
pdxpro0098 <125691886+pdxpro0098@users.noreply.github.com>
2025-04-18 16:57:57 +0530

    Create 11.c

---
+++ Solutions File/11.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+int perfect(int num)
+{
+    int factSum = 0;
+    for (int i = 1; i < num; i++)
+    {
+        if (num % i == 0)
+        {
+            factSum += i;
+        }
+    }
+    if (factSum == num)
+    {
+        printf("perfect square : %d", factSum);
+    }
+}
+int main()
+{
+for (int i = 0; i < 1000; i++)
+{
+    perfect(i);
+}
+
+    return 0;
+}
