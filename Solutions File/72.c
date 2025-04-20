033bb59e64f354791932afe31f812e31207e0ed0
Dalip-Kumar <dalipkumar3559@gmail.com>
2025-04-20 18:12:51 +0530

    Update 72.c

--- Solutions File/72.c
+++ Solutions File/72.c
@@ -1,1 +1,25 @@
 
+#include <stdio.h>
+
+void printIncrementLoop(int a,int b);
+void printDecrementLoop(int a,int b);
+
+int main(){
+    int a,b;
+    printDecrementLoop(35,26);
+
+    return 0;
+}
+
+void printIncrementLoop(int a,int b){
+    for(a;a<=b;a++){
+        printf("%d",a);
+    }
+}
+
+void printDecrementLoop(int b,int a){
+    for(b;b>=a;b--){
+        printf("%d\n",b);
+    }
+}
+
