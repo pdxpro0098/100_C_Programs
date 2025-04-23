867651cdad7f7e0e4d3c00a69541f394d3cb6253
Dalip-Kumar <dalipkumar3559@gmail.com>
2025-04-23 16:13:52 +0530

    Create 93.c

---
+++ Solutions File/93.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int sum(int a,int b);
+
+int main(){
+    int a,b;
+    printf("Enter first Number: ");
+    scanf("%d",&a);
+    printf("Enter second Number: ");
+    scanf("%d",&b);
+    int result = sum(a,b);
+    printf("The sum of %d and %d is %d",a,b,result);
+    return 0;
+}
+int sum(int a,int b){
+    int c = a + b;
+    return c;
+}
