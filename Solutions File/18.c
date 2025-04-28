18103b08a91e0de9b8450f879ea27f20cf74deca
pdxpro0098 <125691886+pdxpro0098@users.noreply.github.com>
2025-04-28 18:09:11 +0530

    Create 18.c

---
+++ Solutions File/18.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <strings.h>
+
+void primeFactors(int num)
+{
+    int i = 2;
+    while (num != 1)
+    {
+        while (num % i == 0)
+        {
+            printf("%d ", i);
+            num /= i;
+        }
+        i++;
+    }
+}
+
+int main()
+{
+    primeFactors(99);
+}
