fdc66dc017e2dd757f1f41d4d5ea89750d9151ff
PDX PRO <pdxpro0098@gmail.com>
2025-05-07 22:10:57 +0530

    add 41.c

---
+++ Solutions File/41.c
@@ -0,0 +1,17 @@
+// 41. Example Class Definition to demonstrate the usage of if else if statement.
+#include <stdio.h>
+
+int main()
+{
+    int a = 6;
+    if (a % 2 == 0)
+    {
+        printf("Number is even");
+    }
+    else
+    {
+        printf("Number is odd");
+    }
+
+    return 0;
+}
