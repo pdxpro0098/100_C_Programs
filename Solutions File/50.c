e11035805011eb8ecc19c89daea86343cbf7edd7
PDX PRO <pdxpro0098@gmail.com>
2025-05-08 22:42:27 +0530

    add 50.c

--- Solutions File/50.c
+++ Solutions File/50.c
@@ -1,1 +1,18 @@
-// 50. Program to print prime factorization.
+#include <stdio.h>
+
+int main()
+{
+    int num = 36;
+    int i = 2;
+    while (num > 1)
+    {
+        while (num % i == 0)
+        {
+            printf("%d ", i);
+            num /= i;
+        }
+        i++;
+    }
+
+    return 0;
+}
