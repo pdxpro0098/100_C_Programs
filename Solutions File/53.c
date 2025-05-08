b0a1fedd307f8a4e6d2efb2a5a2e82471a42c8e4
PDX PRO <pdxpro0098@gmail.com>
2025-05-08 22:37:11 +0530

    ad 53.c

--- Solutions File/53.c
+++ Solutions File/53.c
@@ -1,1 +1,17 @@
-✔️ 53. To print the first ten terms of the fibonacci series.
+#include <stdio.h>
+
+int main()
+{
+    int first = 0;
+    int second = 1;
+    int third;
+    for (int i = 0; i <= 10; i++)
+    {
+        printf("%d ", first);
+        third = first + second;
+        first = second;
+        second = third;
+    }
+
+    return 0;
+}
