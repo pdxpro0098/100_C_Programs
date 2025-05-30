7bdbc853c599fd215e9231755926bf0a485a1e29
Dalip-Kumar <dalipkumar3559@gmail.com>
2025-05-30 15:04:17 +0530

    optimised 29.c

--- Solutions File/29.c
+++ Solutions File/29.c
@@ -1,13 +1,26 @@
 #include <stdio.h>
-#define LEN(arr) (sizeof(arr)) / sizeof(arr[0])
 
-int main()
-{
-    int search = 8;
-    int arr[] = {2, 4, 8, 5};
-    for (int i = 0; i < LEN(arr); i++)
-        if (search == arr[i])
-            printf("%d", i);
+int main(){
+    int size,n,i,j,found = 0;
+    int arr[] = {2,4,5,3,6};
 
+    size = sizeof(arr) / sizeof(arr[0]);
+    
+    printf("Enter the search element: ");
+    scanf("%d",&n);
+
+    for(i = 0;i<size;i++){
+        if(n == arr[i]){
+            found = 1;
+            break;
+        }
+    }
+
+    if(found == 1){
+        printf("Value has been found");
+    }
+    else{
+        printf("Not Found");
+    }
     return 0;
 }
