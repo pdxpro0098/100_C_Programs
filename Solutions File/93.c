6d705763c9d0fc528c85b8d3adfdf9265fe9ba8b
Dalip-Kumar <dalipkumar3559@gmail.com>
2025-05-30 14:06:31 +0530

    Update 93.c

--- Solutions File/93.c
+++ Solutions File/93.c
@@ -1,20 +1,17 @@
 #include <stdio.h>
 
-int sum(int a, int b);
-
 int main()
 {
-    int a, b;
-    printf("Enter first Number: ");
-    scanf("%d", &a);
-    printf("Enter second Number: ");
-    scanf("%d", &b);
-    int result = sum(a, b);
-    printf("The sum of %d and %d is %d", a, b, result);
+    int sum = 0, num;
+
+    printf("Enter the number: ");
+    scanf("%d", &num);
+
+    for (num; num > 0; num /= 10)
+    {
+        sum += num % 10;
+    }
+    printf("%d", sum);
+
     return 0;
-}
-int sum(int a, int b)
-{
-    int c = a + b;
-    return c;
 }
