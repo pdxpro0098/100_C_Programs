bcb4b609f6590f116c3cd92051beec1dc2077570
Dalip-Kumar <dalipkumar3559@gmail.com>
2025-05-31 21:26:58 +0530

    Update 85.c

--- Solutions File/85.c
+++ Solutions File/85.c
@@ -1,14 +1,19 @@
 #include <stdio.h>
-#include <string.h>
-#define size 11
-
+            
 int main()
-{
-
-    char str[size] = "krish dalip";
-    for (int i = 0; i <= size; i++)
     {
-        printf("%c", str[size-i]);
+    int i, j, k = 0;
+    int ar[] = {2, 4, 5, 7, 3, 9};
+
+    for (i = 0, j = 5; i < 6 / 2; i++, j--)
+    {
+        ar[i] = ar[i] + ar[j];
+        ar[j] = ar[i] - ar[j];
+        ar[i] = ar[i] - ar[j];
+    }
+    for (k = 0; k < 6; k++)
+    {
+        printf("%d", ar[k]);
     }
 
     return 0;
