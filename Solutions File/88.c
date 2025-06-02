562e570b1c177b7005d9f228ba31506474e06fc0
Dalip-Kumar <dalipkumar3559@gmail.com>
2025-06-02 17:36:39 +0530

    Optimised 88.c

--- Solutions File/88.c
+++ Solutions File/88.c
@@ -7,15 +7,15 @@ int main()
     int arr[] = {6, 3, 7, 2, 8, 5, 9, 4};
     length = sizeof(arr) / sizeof(arr[0]);
 
-    for (i = 0; i < length; i++)
+    for (i = 0,j=i+1;j,i < length; i++)
     {
         for (j = i + 1; j < length; j++)
         {
             if (arr[i] < arr[j])
             {
-                arr[i] = arr[i] + arr[j];
-                arr[j] = arr[i] - arr[j];
-                arr[i] = arr[i] - arr[j];
+                int temp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = temp;
             }
         }
         printf(" %d", arr[i]);
