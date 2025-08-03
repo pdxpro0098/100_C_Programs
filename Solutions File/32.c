4d9d31b900d1b3b9bdb6cbad00eecfca09b071dd
Dalip-Kumar <dalipkumar3559@gmail.com>
2025-08-03 11:10:28 +0530

    Changes 32.c

--- Solutions File/32.c
+++ Solutions File/32.c
@@ -1,3 +1,4 @@
+// Krish's Approach
 #include <stdio.h>
 
 int main()
@@ -31,6 +32,42 @@ int main()
     }
 
     printf("%d Occur %d Times in Work Array", m, occurence);
+
+    return 0;
+}
+
+
+// Dalip's Approach
+#include <stdio.h>
+
+int findOccurence(int work[],int length, int n)
+{
+    int count = 0;
+    
+    for (int i = 0; i < length; i++)
+    {
+        if (n != work[i])
+        {
+            continue;
+        }
+        else
+        {
+            count++;
+        }
+    }
+    
+    return count;
+}
+
+int main()
+{
+    int work[] = {4, 6, 2, 8, 4, 8, 7, 8};
+    int length = sizeof(work)/sizeof(work[1]);
+    int m;
+    printf("Enter the number: ");
+    scanf("%d",&m);
+    
+    printf("Appear %d times",findOccurence(work,length,m));
 
     return 0;
 }
